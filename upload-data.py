6ead63689511eb17756ae45b62445331d7b7fd5d
Alessio Giori <alessio.giori@graphitech.it>
2016-02-26 18:09:45 +0100

    improved stability with two little time.sleep

--- upload-data.py
+++ upload-data.py
@@ -4,6 +4,8 @@
 
 import httplib
 import datetime
+import time
+import os
 
 # URL or IP and Port of Industrial PC
 url = "192.168.253.100:8080"
@@ -11,14 +13,14 @@ servlet = "/SlopeIndustrialPC"
 
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 # Files paths in production
-# msgFilePath = '/root/slope-data/' + today + '_canbus-messages.txt'
-# tagsFilePath = '/root/slope-data/' + today + '_rfid-tags.txt'
-# logFilePath = '/var/log/slope/' + today + '_upload-data.log'
+msgFilePath = '/root/slope-data/' + today + '_canbus-messages.txt'
+tagsFilePath = '/root/slope-data/' + today + '_rfid-tags.txt'
+logFilePath = '/var/log/slope/' + today + '_upload-data.log'
 
 # Files paths in local development
-msgFilePath = "../example_slope-canbus-messages.txt"
-tagsFilePath = "../example_slope-rfid-tags.txt"
-logFilePath = '../upload-data.log'
+# msgFilePath = "../example_slope-canbus-messages.txt"
+# tagsFilePath = "../example_slope-rfid-tags.txt"
+# logFilePath = '../upload-data.log'
 
 
 def write_log(text):
@@ -28,6 +30,8 @@ def write_log(text):
 	logfile.close()
 
 
+begin = int(round(time.time() * 1000))
+
 try:
 	conn = httplib.HTTPConnection(url)
 	conn.request("GET", servlet)
@@ -35,30 +39,36 @@ try:
 	if conn.getresponse().status == 200:
 		headers = {"Content-type": "text/plain"}
 
-		msgFile = open(msgFilePath, 'r')
-		for msg in msgFile:
-			conn = httplib.HTTPConnection(url)
-			conn.request("POST", servlet, msg, headers)
-			if conn.getresponse().status == 200:
-				write_log('OK: Message sent correctly')
-			else:
-				write_log('Error: ' + conn.getresponse().status + ' ' + conn.getresponse().reason)
+		if os.path.isfile(tagsFilePath):
+			tagsFile = open(tagsFilePath, 'r')
+			for tag in tagsFile:
+				conn = httplib.HTTPConnection(url)
+				conn.request("POST", servlet + "/tags/upload", tag, headers)
+				if conn.getresponse().status != 200:
+					write_log('Error: ' + conn.getresponse().status + ' ' + conn.getresponse().reason)
+				time.sleep(0.005)
 
-		msgFile.close()
+			tagsFile.close()
+			os.rename(tagsFilePath, tagsFilePath + ".done")
 
-		tagsFile = open(tagsFilePath, 'r')
-		for tag in tagsFile:
-			conn = httplib.HTTPConnection(url)
-			conn.request("POST", servlet, tag, headers)
-			if conn.getresponse().status == 200:
-				write_log('OK: Message sent correctly')
-			else:
-				write_log('Error: ' + conn.getresponse().status + ' ' + conn.getresponse().reason)
+		if os.path.isfile(msgFilePath):
+			msgFile = open(msgFilePath, 'r')
+			for msg in msgFile:
+				conn = httplib.HTTPConnection(url)
+				conn.request("POST", servlet + "/canmsg/upload", msg, headers)
+				if conn.getresponse().status != 200:
+					write_log('Error: ' + conn.getresponse().status + ' ' + conn.getresponse().reason)
+				time.sleep(0.001)
 
-		tagsFile.close()
+			msgFile.close()
+			os.rename(msgFilePath, msgFilePath + ".done")
 
 except IOError:
 	write_log('Error: Industrial PC unreachable')
 
 finally:
 	conn.close()
+
+end = int(round(time.time() * 1000))
+
+print end - begin
