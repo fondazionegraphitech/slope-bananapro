b8879034349779334f3b7e57e25eb340b5b5c6fb
Umberto Di Staso <umberto.di.staso@graphitech.it>
2016-05-26 17:39:43 +0200

    added timestamp to .done files added comments

--- upload-data.py
+++ upload-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# upload CAN messages and Tags detected to Industrial PC via WiFi
+# Upload CAN messages and Tags detected to Industrial PC via WiFi
+# This script will be called every 5 second by the main script
 
 import httplib
 import datetime
@@ -11,26 +12,22 @@ import os
 url = "127.0.0.1:80"
 servlet = "/BananaProServer/index.php"
 
+# Paths to the LOG files: only _canbus-messages and _rfid-tags data will be sent
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 msgFilePath = '/root/slope-data/' + today + '_canbus-messages.txt'
 tagsFilePath = '/root/slope-data/' + today + '_rfid-tags.txt'
 logFilePath = '/root/slope-log/' + today + '_slope-upload.log'
-
-# Files paths in local development
-# msgFilePath = "../example_slope-canbus-messages.txt"
-# tagsFilePath = "../example_slope-rfid-tags.txt"
-# logFilePath = '../upload-data.log'
-
 
 def write_log(text):
 	logfile = open(logFilePath, 'a', 1)
 	now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 	logfile.write('[' + now + '] ' + text + '\n')
 	logfile.close()
-
-#write_log('Slope-Upload Version 1.0')
+	
+timestamp =  str(int(round(time.time() * 1000)))	
 
 try:
+	# Within this code we are trying to check if the industrial pc is reachable
 	conn = httplib.HTTPConnection(url)
 	conn.request("GET", servlet)
 
@@ -38,6 +35,7 @@ try:
 		write_log('Connection with the industrial pc established, send data')
 		headers = {"Content-type": "text/plain"}
 
+		# First, we will send TAGS data: one POST call for each Json stored in _rfid-tags.txt
 		if os.path.isfile(tagsFilePath):
 			tagsFile = open(tagsFilePath, 'r')
 			for tag in tagsFile:
@@ -48,12 +46,13 @@ try:
 				text = response.read()
 				if status != 200:
 					write_log('Error: ' + status + ' ' + response.reason)
-				print text
 				time.sleep(0.005)	
 
 			tagsFile.close()
-			os.rename(tagsFilePath, tagsFilePath + ".done")
+			# Then, we will rename _rfid-tags.txt in order to avoid to send same data in the future
+			os.rename(tagsFilePath, tagsFilePath + "." + timestamp + ".done")
 
+		# Now, we will send CANBUS data: one POST call for each Json stored in _canbus-messages.txt
 		if os.path.isfile(msgFilePath):
 			msgFile = open(msgFilePath, 'r')
 			for msg in msgFile:
@@ -64,11 +63,11 @@ try:
 				text = response.read()
 				if status != 200:
 					write_log('Error: ' + status + ' ' + response.reason)
-				print text	
 				time.sleep(0.005)
 
 			msgFile.close()
-			os.rename(msgFilePath, msgFilePath + ".done")
+			# Then, we will rename _canbus-messages.txt in order to avoid to send same data in the future
+			os.rename(msgFilePath, msgFilePath + "." + timestamp + ".done")
 
 except IOError:
 	write_log('Error: Industrial PC unreachable')
